e64a32bd4e679f5d210d5ed84efea8a9773cc2b6
solcabz <xhzoul30@gmail.com>
2024-03-07 21:28:04 +0800

    login img src

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -56,7 +56,7 @@ function Login({ onLogin }) {
       <div
         className="bg-center bg-no-repeat bg-cover h-screen w-screen p-0"
         style={{
-          backgroundImage: `url('./src/assets/LoginBG.svg')`,
+          backgroundImage: `url('/LoginBG.svg')`,
         }}
       >
         <form
