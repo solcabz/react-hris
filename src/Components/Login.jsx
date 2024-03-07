37ca50000b5338eebc82633a30115e72451011e8
solcabz <xhzoul30@gmail.com>
2024-03-07 21:24:22 +0800

    login img src

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -56,7 +56,7 @@ function Login({ onLogin }) {
       <div
         className="bg-center bg-no-repeat bg-cover h-screen w-screen p-0"
         style={{
-          backgroundImage: `url('https://github.com/solcabz/react-hris/tree/main/src/assets/LoginBG.png')`,
+          backgroundImage: `url('./src/assets/LoginBG.svg')`,
         }}
       >
         <form
@@ -70,7 +70,7 @@ function Login({ onLogin }) {
             }}
           >
             <img
-              src="https://github.com/solcabz/react-hris/tree/main/src/assets/TNGCLogo.png"
+              src="./src/assets/TNGCLogo.png"
               alt="TNGC Logo"
               className="w-[110px] h-[120px]"
             />
