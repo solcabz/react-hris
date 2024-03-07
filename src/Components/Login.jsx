afb18a036f8e0e54587ec66fb72e978bcd78c40b
solcabz <xhzoul30@gmail.com>
2024-03-07 21:36:55 +0800

    login img src

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -56,7 +56,7 @@ function Login({ onLogin }) {
       <div
         className="bg-center bg-no-repeat bg-cover h-screen w-screen p-0"
         style={{
-          backgroundImage: `url('/LoginBG.svg')`,
+          backgroundImage: `url('../assets/LoginBG.svg')`,
         }}
       >
         <form
@@ -70,7 +70,7 @@ function Login({ onLogin }) {
             }}
           >
             <img
-              src="./src/assets/TNGCLogo.png"
+              src="../assets/TNGCLogo.png"
               alt="TNGC Logo"
               className="w-[110px] h-[120px]"
             />
